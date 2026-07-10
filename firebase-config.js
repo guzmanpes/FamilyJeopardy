515712b83675677016b225abf3b77750b6d48caa
guzmanpes <gabriel_guzman@snowlineschools.com>
2026-07-10 12:42:11 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyClGQiofKzvGaM5Z30oVhq9ysT0VrGRzaQ",
+  authDomain: "family-jeopardy-33dc9.firebaseapp.com",
+  databaseURL: "https://family-jeopardy-33dc9-default-rtdb.firebaseio.com",
+  projectId: "family-jeopardy-33dc9",
+  storageBucket: "family-jeopardy-33dc9.firebasestorage.app",
+  messagingSenderId: "981923161817",
+  appId: "1:981923161817:web:b4a296df6f9b02cc9bc0c8"
+};
