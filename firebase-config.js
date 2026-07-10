56fff64a0805b86fbe4a2774d65422e4ed96ee4e
guzmanpes <gabriel_guzman@snowlineschools.com>
2026-07-10 11:58:37 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT",
-  storageBucket: "PASTE_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyClGQiofKzvGaM5Z30oVhq9ysT0VrGRzaQ",
+  authDomain: "family-jeopardy-33dc9.firebaseapp.com",
+  projectId: "family-jeopardy-33dc9",
+  storageBucket: "family-jeopardy-33dc9.firebasestorage.app",
+  messagingSenderId: "981923161817",
+  appId: "1:981923161817:web:b4a296df6f9b02cc9bc0c8"
 };
