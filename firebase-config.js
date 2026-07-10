ecea2b8b3f863594b310f199fc067b32f4894261
guzmanpes <gabriel_guzman@snowlineschools.com>
2026-07-10 11:52:17 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyClGQiofKzvGaM5Z30oVhq9ysT0VrGRzaQ",
+    authDomain: "family-jeopardy-33dc9.firebaseapp.com",
+    projectId: "family-jeopardy-33dc9",
+    storageBucket: "family-jeopardy-33dc9.firebasestorage.app",
+    messagingSenderId: "981923161817",
+    appId: "1:981923161817:web:b4a296df6f9b02cc9bc0c8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
